0d8f61ac09eade4accb4f71f57eda22ed1b667b3
Matthew Palm <mattdpalm@gmail.com>
2023-09-26 10:58:05 -0400

    adding A/B regression

--- TestCode2.R
+++ TestCode2.R
@@ -6,3 +6,8 @@ d.. <- read.csv(file="Data.csv")
 d..$W <-  d..$X/d..$Y
 
 #I dont know whats going on anymore. I am confused.  When I was a child, all we did was be boomers.
+
+A <- c(2,6,8,29,1,6)
+B <- c(9,1,3,-3,19,2)
+
+summary(lm(B~A))
